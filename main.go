de03df2c76bc186662a315679aeda0dae06bc42d
JulianMunguia04 <julianmunguia04@gmail.com>
2026-03-02 17:11:58 -0700

    Fix double request on favico

--- main.go
+++ main.go
@@ -12,6 +12,8 @@ var (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Path:", r.URL.Path)
+
 	mu.Lock()
 	counter++
 	val := counter
@@ -22,5 +24,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {	//Avoid double request
+		http.NotFound(w, r)
+	})
 	http.ListenAndServe(":8080", nil)
 }
