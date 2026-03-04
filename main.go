b30971702e10ad8a24da34743fd440518a8361bc
JulianMunguia04 <julianmunguia04@gmail.com>
2026-03-03 23:43:34 -0700

    Reverse Proxy

--- main.go
+++ main.go
@@ -1,31 +1,75 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"sync"
 )
 
-var (
-	counter int
-	mu      sync.Mutex
-)
+type Backend struct {
+	URL *url.URL
+}
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Path:", r.URL.Path)
+type LoadBalancer struct {
+	backends []*Backend
+	mu       sync.Mutex
+	current  int
+}
 
-	mu.Lock()
-	counter++
-	val := counter
-	mu.Unlock()
+func (lb *LoadBalancer) NextBackend() *Backend {
+	lb.mu.Lock()
+	defer lb.mu.Unlock()
 
-	fmt.Fprintf(w, "Count: %d\n", val)
+	backend := lb.backends[lb.current]
+	lb.current = (lb.current + 1) % len(lb.backends)
+
+	return backend
+}
+
+func newLoadBalancer(addresses []string) *LoadBalancer {
+	var backends []*Backend
+
+	for _, addr := range addresses {
+		parsed, err := url.Parse(addr)
+		if err != nil {
+			log.Fatal(err)
+		}
+		backends = append(backends, &Backend{URL: parsed})
+	}
+
+	return &LoadBalancer{
+		backends: backends,
+	}
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {	//Avoid double request
-		http.NotFound(w, r)
+
+	app1LB := newLoadBalancer([]string{
+		"http://localhost:5000",
+		"http://localhost:5001",
 	})
-	http.ListenAndServe(":8080", nil)
+
+	app2LB := newLoadBalancer([]string{
+		"http://localhost:3000",
+		"http://localhost:3001",
+	})
+
+	http.HandleFunc("/app1/", func(w http.ResponseWriter, r *http.Request) {
+		backend := app1LB.NextBackend()
+		proxy := httputil.NewSingleHostReverseProxy(backend.URL)
+		proxy.ServeHTTP(w, r)
+	})
+
+	http.HandleFunc("/app2/", func(w http.ResponseWriter, r *http.Request) {
+		backend := app2LB.NextBackend()
+		proxy := httputil.NewSingleHostReverseProxy(backend.URL)
+		proxy.ServeHTTP(w, r)
+	})
+
+	log.Println("Gateway running on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
+
+//go run main.go
