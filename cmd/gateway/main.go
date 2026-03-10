11c30b3dfcb7da7867efd5b4ada426bf8743a29d
JulianMunguia04 <julianmunguia04@gmail.com>
2026-03-10 00:50:46 -0600

    Refactor, Round Robin

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -3,73 +3,28 @@ package main
 import (
 	"log"
 	"net/http"
-	"net/http/httputil"
-	"net/url"
-	"sync"
+
+	"gateway/internal/loadbalancer"
+	"gateway/internal/proxy"
 )
-
-type Backend struct {
-	URL *url.URL
-}
-
-type LoadBalancer struct {
-	backends []*Backend
-	mu       sync.Mutex
-	current  int
-}
-
-func (lb *LoadBalancer) NextBackend() *Backend {
-	lb.mu.Lock()
-	defer lb.mu.Unlock()
-
-	backend := lb.backends[lb.current]
-	lb.current = (lb.current + 1) % len(lb.backends)
-
-	return backend
-}
-
-func newLoadBalancer(addresses []string) *LoadBalancer {
-	var backends []*Backend
-
-	for _, addr := range addresses {
-		parsed, err := url.Parse(addr)
-		if err != nil {
-			log.Fatal(err)
-		}
-		backends = append(backends, &Backend{URL: parsed})
-	}
-
-	return &LoadBalancer{
-		backends: backends,
-	}
-}
 
 func main() {
 
-	app1LB := newLoadBalancer([]string{
+	app1LB := loadbalancer.NewRoundRobin([]string{
 		"http://localhost:5000",
 		"http://localhost:5001",
+		"http://localhost:5002",
 	})
 
-	app2LB := newLoadBalancer([]string{
+	app2LB := loadbalancer.NewRoundRobin([]string{
 		"http://localhost:3000",
 		"http://localhost:3001",
 	})
 
-	http.HandleFunc("/app1/", func(w http.ResponseWriter, r *http.Request) {
-		backend := app1LB.NextBackend()
-		proxy := httputil.NewSingleHostReverseProxy(backend.URL)
-		proxy.ServeHTTP(w, r)
-	})
-
-	http.HandleFunc("/app2/", func(w http.ResponseWriter, r *http.Request) {
-		backend := app2LB.NextBackend()
-		proxy := httputil.NewSingleHostReverseProxy(backend.URL)
-		proxy.ServeHTTP(w, r)
-	})
+	http.HandleFunc("/app1/", proxy.ProxyRequest(app1LB))
+	http.HandleFunc("/app2/", proxy.ProxyRequest(app2LB))
 
 	log.Println("Gateway running on :8080")
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
-
-//go run main.go
