75abc2691f8731f5c36b0106227f3789d8ced79c
JulianMunguia04 <julianmunguia04@gmail.com>
2026-03-27 13:01:04 -0600

    Revert "Kubernetes"
    This reverts commit 86473419d0e6a26c8ac61c1357ece7accbab327e.

--- internal/ratelimiter/redis_limiter.go
+++ internal/ratelimiter/redis_limiter.go
@@ -2,29 +2,19 @@ package ratelimiter
 
 import (
 	"context"
-	"os"
 	"time"
 
 	"github.com/redis/go-redis/v9"
 )
 
 var ctx = context.Background()
-var rdb *redis.Client
 
-func init() {
-	redisAddr := os.Getenv("REDIS_ADDR")
-	if redisAddr == "" {
-		redisAddr = "redis:6379"
-	}
-
-	rdb = redis.NewClient(&redis.Options{
-		Addr:     redisAddr,
-		Password: "",
-		DB:       0,
-	})
-}
+var rdb = redis.NewClient(&redis.Options{
+	Addr: "redis:6379",
+})
 
 func AllowRequest(ip string, limit int, window time.Duration) (bool, error) {
+
 	key := "rate_limit:" + ip
 
 	count, err := rdb.Incr(ctx, key).Result()
